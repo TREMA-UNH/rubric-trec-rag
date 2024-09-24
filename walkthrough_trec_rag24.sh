0afee2bdc7ab3fe1fb48db967b0e64e8e2eccb80
Laura <dietz@cs.unh.edu>
2024-09-24 12:01:20 -0400

    walkthrough script

--- walkthrough_trec_rag24.sh
+++ walkthrough_trec_rag24.sh
@@ -24,26 +24,26 @@ DATA="./data"
 # 
 # The following files are produced:
 #
-# rag-questions.jsonl.gz: Generated exam questions
+# rag24-questions.jsonl.gz: Generated exam questions
 #
-# rag-nuggets.jsonl.gz Generated test nuggets
+# rag24-nuggets.jsonl.gz Generated test nuggets
 
 
 
-# echo -e "\n\n\nGenerate rag Nuggets"
+# echo -e "\n\n\nGenerate rag24 Nuggets"
 
-#python -O -m exam_pp.question_generation -q $DATA/rag-queries.json -o $DATA/rag-nuggets.jsonl.gz --use-nuggets --test-collection rag --description "A new set of generated nuggets for rag"
+#python -O -m exam_pp.question_generation -q $DATA/rag24-queries.json -o $DATA/rag24-nuggets.jsonl.gz --use-nuggets --test-collection rag24 --description "A new set of generated nuggets for rag24"
 
-# echo -e "\n\n\Generate rag Questions"
+# echo -e "\n\n\Generate rag24 Questions"
 
-# python -O -m exam_pp.question_generation -q $DATA/rag_queries_dev.jsonl -o $DATA/rag-questions_dev.jsonl.gz --test-collection rag_dev --description "A new set of generated questions for rag dev set"
+# python -O -m exam_pp.question_generation -q $DATA/rag_queries_dev.jsonl -o $DATA/rag24-questions_dev.jsonl.gz --test-collection rag_dev --description "A new set of generated questions for rag24 dev set"
 
 
-# python -O -m exam_pp.question_generation -q $DATA/rag_queries_test.jsonl -o $DATA/rag-questions_test.jsonl.gz --test-collection rag_test --description "A new set of generated questions for rag test set"
+# python -O -m exam_pp.question_generation -q $DATA/rag_queries_test.jsonl -o $DATA/rag24-questions_test.jsonl.gz --test-collection rag_test --description "A new set of generated questions for rag24 test set"
 
 echo -e "\n\n\n Exam Grading"
 
-ungraded="rubric-rag-retrieval.jsonl.gz"
+ungraded="rubric-rag24-retrieval.jsonl.gz"
 
 
 ### Phase 2: Grading
@@ -69,27 +69,27 @@ zcat $DATA/$ungraded | wc -l
 
 # grade nuggets
 
-# 	python -O -m exam_pp.exam_grading $DATA/$ungraded -o $DATA/$withrate --model-pipeline text2text --model-name google/flan-t5-large --prompt-class NuggetSelfRatedPrompt --question-path $DATA/rag-nuggets.jsonl.gz  --question-type question-bank --use-nuggets  
+# 	python -O -m exam_pp.exam_grading $DATA/$ungraded -o $DATA/$withrate --model-pipeline text2text --model-name google/flan-t5-large --prompt-class NuggetSelfRatedPrompt --question-path $DATA/rag24-nuggets.jsonl.gz  --question-type question-bank --use-nuggets  
 
-# echo -e "\n\n\ Explained rag Nuggets"
+# echo -e "\n\n\ Explained rag24 Nuggets"
 
-# 	python -O -m exam_pp.exam_grading $DATA/$withrate  -o $DATA/$withrateextract --model-pipeline text2text --model-name google/flan-t5-large --prompt-class NuggetExtractionPrompt --question-path $DATA/rag-nuggets.jsonl.gz  --question-type question-bank --use-nuggets  
+# 	python -O -m exam_pp.exam_grading $DATA/$withrate  -o $DATA/$withrateextract --model-pipeline text2text --model-name google/flan-t5-large --prompt-class NuggetExtractionPrompt --question-path $DATA/rag24-nuggets.jsonl.gz  --question-type question-bank --use-nuggets  
 
 # grade questions
 
-echo -e "\n\n\ Rated rag Questions"
+echo -e "\n\n\ Rated rag24 Questions"
 # ungraded="$withrateextract"
 withrate="questions-rate--${ungraded}"
 withrateextract="questions-explain--${withrate}"
 
 
-time python -O -m exam_pp.exam_grading $DATA/$ungraded -o $DATA/$withrate --model-pipeline text2text --model-name google/flan-t5-large --prompt-class QuestionSelfRatedUnanswerablePromptWithChoices --question-path $DATA/rag-questions.jsonl.gz  --question-type question-bank  --max-queries 1
+time python -O -m exam_pp.exam_grading $DATA/$ungraded -o $DATA/$withrate --model-pipeline text2text --model-name google/flan-t5-large --prompt-class QuestionSelfRatedUnanswerablePromptWithChoices --question-path $DATA/rag24-questions.jsonl.gz  --question-type question-bank  --max-queries 1
 
 
 
-echo -e "\n\n\ Explained rag Questions"
+echo -e "\n\n\ Explained rag24 Questions"
 
-# 	python -O -m exam_pp.exam_grading $DATA/$withrate  -o $DATA/$withrateextract --model-pipeline text2text --model-name google/flan-t5-large --prompt-class QuestionCompleteConciseUnanswerablePromptWithChoices --question-path $DATA/rag-questions_${subset}.jsonl.gz  --question-type question-bank 
+# 	python -O -m exam_pp.exam_grading $DATA/$withrate  -o $DATA/$withrateextract --model-pipeline text2text --model-name google/flan-t5-large --prompt-class QuestionCompleteConciseUnanswerablePromptWithChoices --question-path $DATA/rag24-questions_${subset}.jsonl.gz  --question-type question-bank 
 
 
 final=$withrateextract
@@ -103,15 +103,15 @@ echo "Graded: $final"
 #
 # the files produced in this phase are:
 # dl-verify-grading.txt : answers to the grading propts selfrated/extraction (grouped by question/nugget)
-# rag-bad-question.txt : Questions/nuggets frequently covered by non-relevant passages (should be removed from the test bank)
-# rag-uncovered-passages.txt : Relevant passages not covered by any question/nugget (require the addition of new test nuggets/questions.
+# rag24-bad-question.txt : Questions/nuggets frequently covered by non-relevant passages (should be removed from the test bank)
+# rag24-uncovered-passages.txt : Relevant passages not covered by any question/nugget (require the addition of new test nuggets/questions.
 #
 
-#python -O -m exam_pp.exam_verification --verify-grading $DATA/$final  --question-path $DATA/rag-questions.jsonl.gz  --question-type question-bank  > $DATA/rag-verify-grading.txt
+#python -O -m exam_pp.exam_verification --verify-grading $DATA/$final  --question-path $DATA/rag24-questions.jsonl.gz  --question-type question-bank  > $DATA/rag24-verify-grading.txt
 
-#python -O -m exam_pp.exam_verification --uncovered-passages $DATA/$final --question-path $DATA/rag-questions.jsonl.gz  --question-type question-bank --min-judgment 1 --min-rating 4 > $DATA/rag-uncovered-passages.txt
+#python -O -m exam_pp.exam_verification --uncovered-passages $DATA/$final --question-path $DATA/rag24-questions.jsonl.gz  --question-type question-bank --min-judgment 1 --min-rating 4 > $DATA/rag24-uncovered-passages.txt
 
-#python -O -m exam_pp.exam_verification --bad-question $DATA/$final  --question-path $DATA/rag-questions.jsonl.gz  --question-type question-bank --min-judgment 1 --min-rating 4  >  $DATA/rag-bad-question.txt
+#python -O -m exam_pp.exam_verification --bad-question $DATA/$final  --question-path $DATA/rag24-questions.jsonl.gz  --question-type question-bank --min-judgment 1 --min-rating 4  >  $DATA/rag24-bad-question.txt
 
 
 
@@ -127,12 +127,12 @@ echo "Graded: $final"
 #
 # The files produced in this phase are:
 #
-# rag-autograde-qrels-\$promptclass-minrating-4.solo.qrels:  Exported Qrel file treating passages with self-ratings >=4 
+# rag24-autograde-qrels-\$promptclass-minrating-4.solo.qrels:  Exported Qrel file treating passages with self-ratings >=4 
 #
-# rag-autograde-qrels-leaderboard-\$promptclass-minrating-4.solo.tsv:  Leaderboard produced with 
+# rag24-autograde-qrels-leaderboard-\$promptclass-minrating-4.solo.tsv:  Leaderboard produced with 
 #        trec_eval using the exported Qrel file
 #
-# rag-autograde-cover-leaderboard-\$promptclass-minrating-4.solo.tsv: Leaderboads produced with Autograde Cover treating \
+# rag24-autograde-cover-leaderboard-\$promptclass-minrating-4.solo.tsv: Leaderboads produced with Autograde Cover treating \
 # 	test nuggets/questions as answered when any passage obtains a self-ratings >= 4
 #
 #
@@ -143,13 +143,13 @@ for promptclass in  QuestionSelfRatedUnanswerablePromptWithChoices ; do  #Nugget
 
 	for minrating in 3 4 5; do
 		echo ""
-		# python -O -m exam_pp.exam_evaluation $DATA/$final --question-set question-bank --prompt-class $promptclass --min-self-rating $minrating --leaderboard-out $DATA/rag-autograde-cover-leaderboard-$promptclass-minrating-$minrating.solo.$ungraded.tsv 
+		# python -O -m exam_pp.exam_evaluation $DATA/$final --question-set question-bank --prompt-class $promptclass --min-self-rating $minrating --leaderboard-out $DATA/rag24-autograde-cover-leaderboard-$promptclass-minrating-$minrating.solo.$ungraded.tsv 
 
-		# N.B. requires TREC-rag runs to be populated in $DATA/ragruns
-		# python -O -m exam_pp.exam_evaluation $DATA/$final --question-set question-bank --prompt-class $promptclass -q $DATA/rag-autograde-qrels-leaderboard-$promptclass-minrating-$minrating.solo.$ungraded.qrels  --min-self-rating $minrating --qrel-leaderboard-out $DATA/rag-autograde-qrels-$promptclass-minrating-$minrating.solo.$ungraded.tsv --run-dir $DATA/ragruns 
+		# N.B. requires TREC-rag24 runs to be populated in $DATA/ragruns
+		# python -O -m exam_pp.exam_evaluation $DATA/$final --question-set question-bank --prompt-class $promptclass -q $DATA/rag24-autograde-qrels-leaderboard-$promptclass-minrating-$minrating.solo.$ungraded.qrels  --min-self-rating $minrating --qrel-leaderboard-out $DATA/rag24-autograde-qrels-$promptclass-minrating-$minrating.solo.$ungraded.tsv --run-dir $DATA/ragruns 
         
 		# Since generative IR systems will not share any passages, we represent them as special run files
-		#python -O -m exam_pp.exam_evaluation $DATA/$final --question-set question-bank --prompt-class $promptclass -q $DATA/rag-autograde-qrels-leaderboard-$promptclass-minrating-$minrating.solo.$ungraded.qrels  --min-self-rating $minrating --qrel-leaderboard-out $DATA/rag-autograde-qrels-$promptclass-minrating-$minrating.solo.$ungraded.gen.tsv --run-dir $DATA/raggen-runs 
+		#python -O -m exam_pp.exam_evaluation $DATA/$final --question-set question-bank --prompt-class $promptclass -q $DATA/rag24-autograde-qrels-leaderboard-$promptclass-minrating-$minrating.solo.$ungraded.qrels  --min-self-rating $minrating --qrel-leaderboard-out $DATA/rag24-autograde-qrels-$promptclass-minrating-$minrating.solo.$ungraded.gen.tsv --run-dir $DATA/raggen-runs 
 	done
 done
 
@@ -161,19 +161,19 @@ done
 #
 # For DL, where the judgment 1 is a non-relevant grade, the option `--min-relevant-judgment 2` must be used (default is 1)
 #
-# Produced outputs `rag*.correlation.tsv` are leaderboards with rank correlation information (Spearman's rank correlation and Kendall's tau correlation)
+# Produced outputs `rag24*.correlation.tsv` are leaderboards with rank correlation information (Spearman's rank correlation and Kendall's tau correlation)
 #
 #
 # When manual relevance judgments are available Cohen's kappa inter-annotator agreement can be computed. 
 # Manual judgments will be taken from the entries `paragraph_data.judgents[].relevance`
 # 
 # The produced output is
-# rag-autograde-inter-annotator-\$promptclass.tex:  LaTeX tables with graded and binarized inter-annotator statistics with Cohen's kappa agreement. ``Min-anwers'' refers to the number of correct answers obtained above a self-rating threshold by a passage. (For \dl{} –-min-relevant-judgment 2 must be set.)
+# rag24-autograde-inter-annotator-\$promptclass.tex:  LaTeX tables with graded and binarized inter-annotator statistics with Cohen's kappa agreement. ``Min-anwers'' refers to the number of correct answers obtained above a self-rating threshold by a passage. (For \dl{} –-min-relevant-judgment 2 must be set.)
 # 
 
-# python -O -m exam_pp.exam_leaderboard_analysis $DATA/$final  --question-set question-bank --prompt-class  QuestionSelfRatedUnanswerablePromptWithChoices NuggetSelfRatedPrompt Thomas FagB FagB_few HELM Sun Sun_few --min-relevant-judgment 2 --trec-eval-metric ndcg_cut.10 ndcg_cut.20 map Rprec recip_rank  --use-ratings --qrel-dir=$DATA --qrel-analysis-out $DATA/rag-autograde-qrels-leaderboard-analysis-graded.correlation.tsv --run-dir $DATA/ragruns --official-leaderboard $DATA/official_rag_leaderboard.json --question-set-for-facets question-bank --cover-analysis-out $DATA/rag-autograde-cover-leaderboard-analysis-graded.correlation.tsv
+# python -O -m exam_pp.exam_leaderboard_analysis $DATA/$final  --question-set question-bank --prompt-class  QuestionSelfRatedUnanswerablePromptWithChoices NuggetSelfRatedPrompt Thomas FagB FagB_few HELM Sun Sun_few --min-relevant-judgment 2 --trec-eval-metric ndcg_cut.10 ndcg_cut.20 map Rprec recip_rank  --use-ratings --qrel-dir=$DATA --qrel-analysis-out $DATA/rag24-autograde-qrels-leaderboard-analysis-graded.correlation.tsv --run-dir $DATA/ragruns --official-leaderboard $DATA/official_rag_leaderboard.json --question-set-for-facets question-bank --cover-analysis-out $DATA/rag24-autograde-cover-leaderboard-analysis-graded.correlation.tsv
 
-final="questions-explain--questions-rate--rag-passages_dev.json.gz"
+final="questions-explain--questions-rate--rag24-passages_dev.json.gz"
 
 for promptclass in  QuestionSelfRatedUnanswerablePromptWithChoices; do
 	echo $promptclass
@@ -181,17 +181,17 @@ for promptclass in  QuestionSelfRatedUnanswerablePromptWithChoices; do
 	for minrating in 3 4 5; do
 		# autograde-qrels
 		# qrel leaderboard correlation
-		# N.B. requires TREC-rag runs to be populated in $DATA/ragruns
-		#python -O -m exam_pp.exam_post_pipeline $DATA/$final  --question-set question-bank --prompt-class $promptclass  --min-relevant-judgment 2 --use-ratings --min-trec-eval-level ${minrating} -q $DATA/rag-exam-$promptclass.qrel --qrel-leaderboard-out $DATA/rag-autograde-qrels-leaderboard-$promptclass-minlevel-$minrating.correlation.tsv --run-dir $DATA/ragruns --official-leaderboard $DATA/official_rag_leaderboard.json 
+		# N.B. requires TREC-rag24 runs to be populated in $DATA/ragruns
+		#python -O -m exam_pp.exam_post_pipeline $DATA/$final  --question-set question-bank --prompt-class $promptclass  --min-relevant-judgment 2 --use-ratings --min-trec-eval-level ${minrating} -q $DATA/rag24-exam-$promptclass.qrel --qrel-leaderboard-out $DATA/rag24-autograde-qrels-leaderboard-$promptclass-minlevel-$minrating.correlation.tsv --run-dir $DATA/ragruns --official-leaderboard $DATA/official_rag_leaderboard.json 
 	
 		# autograde-cover 
-		 #python -O -m exam_pp.exam_post_pipeline $DATA/$final  --question-set question-bank --prompt-class $promptclass  --min-relevant-judgment 2 --use-ratings --min-self-rating ${minrating} --leaderboard-out $DATA/rag-autograde-cover-leaderboard-$promptclass-minlevel-$minrating.correlation.tsv  --official-leaderboard $DATA/official_rag_leaderboard.json
+		 #python -O -m exam_pp.exam_post_pipeline $DATA/$final  --question-set question-bank --prompt-class $promptclass  --min-relevant-judgment 2 --use-ratings --min-self-rating ${minrating} --leaderboard-out $DATA/rag24-autograde-cover-leaderboard-$promptclass-minlevel-$minrating.correlation.tsv  --official-leaderboard $DATA/official_rag_leaderboard.json
 		echo ""
 	done
 
 
 
 	# inter-annotator agreement
-	# python -O -m exam_pp.exam_post_pipeline $DATA/$final  --question-set question-bank --prompt-class $promptclass  --min-relevant-judgment 2 --use-ratings  --inter-annotator-out $DATA/rag-autograde-inter-annotator-$promptclass.tex
+	# python -O -m exam_pp.exam_post_pipeline $DATA/$final  --question-set question-bank --prompt-class $promptclass  --min-relevant-judgment 2 --use-ratings  --inter-annotator-out $DATA/rag24-autograde-inter-annotator-$promptclass.tex
 done
 
