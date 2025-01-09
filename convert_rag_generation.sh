ce097b954d735e8b37f6e521bb5377ee9c294b33
Laura Dietz (on peanut) <dietz@cs.unh.edu>
2025-01-08 22:11:37 -0500

    bump rubric

--- convert_rag_generation.sh
+++ convert_rag_generation.sh
@@ -5,9 +5,21 @@ set -eo pipefail
 
 echo "auggen"
 
-python -O -m rubric_trec_rag.rag_generation_inputs  -p ./data/rubric-rag24-auggen.jsonl.gz  ./data/auggen/*
+#python -O -m rubric_trec_rag.rag_generation_inputs  -p ./data/rubric-rag24-auggen.jsonl.gz  ./data/auggen/*
 
 echo ""
 echo "gen"
 
-python -O -m rubric_trec_rag.rag_generation_inputs  -p ./data/rubric-rag24-gen.jsonl.gz  ./data/gen/*
+#python -O -m rubric_trec_rag.rag_generation_inputs  -p ./data/rubric-rag24-gen.jsonl.gz  ./data/gen/*
+
+
+
+
+echo "auggen"
+
+python -O -m rubric_trec_rag.rag_generation_inputs  -p ./data/rubric-rag24-concat-auggen.jsonl.gz  ./data/auggen/* --concat-response
+
+echo ""
+echo "gen"
+
+python -O -m rubric_trec_rag.rag_generation_inputs  -p ./data/rubric-rag24-concat-gen.jsonl.gz  ./data/gen/* --concat-response
--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.dspy-nix.url = "git+https://git.smart-cactus.org/ben/dspy-nix";
   inputs.exampp.url = "git+https://github.com/TREMA-UNH/rubric-grading-workbench.git";
+  inputs.dspy-nix.follows = "exampp/dspy-nix";
  
   outputs = inputs@{ self, nixpkgs, flake-utils, dspy-nix, exampp, ... }:
     flake-utils.lib.eachDefaultSystem (system: 
@@ -30,7 +31,7 @@
     );
 
   nixConfig = {
-    substituters = [ "https://dspy-nix.cachix.org" ];
-    trusted-public-keys = [ "dspy-nix.cachix.org-1:VJ553D0iJVoA8ov2+ly+dLnGHarfSQpemzVW6dY6CfE=" ];
+    substituters = [ "https://cache.nixos.org" "https://dspy-nix.cachix.org" ];
+    trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "dspy-nix.cachix.org-1:VJ553D0iJVoA8ov2+ly+dLnGHarfSQpemzVW6dY6CfE=" ];
   };
 }
